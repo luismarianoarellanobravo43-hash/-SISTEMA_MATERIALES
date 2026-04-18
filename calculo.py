49079cacb6abad9743adca2acdbc2b5f0c9686c0
Luis Mariano <luismarianoarellanobravo43@gmail.com>
2026-04-17 20:24:57 -0500

    V3: Agregada validación condicional de hardware

--- calculo.py
+++ calculo.py
@@ -15,3 +15,21 @@ costo_ml = float(input("Ingrese el costo de la resina por ml ($): "))
 costo_total = volumen * costo_ml
 
 print(f"\nEl costo total de manufactura es: ${costo_total:.2f}")
+
+# Versión 3: Validador de Impresión [cite: 47, 48]
+print("---------------------------------------------------------")
+print("+++++++++++++++++++ 3D SERVICES S.A. ++++++++++++++++++++")
+print("++++++++++++ SISTEMA DE CÁLCULO DE MATERIALES +++++++++++")
+print("---------------------------------------------------------")
+
+capacidad_maxima = 500.0
+volumen = float(input("Ingrese el volumen de la pieza (ml): "))
+
+if volumen > capacidad_maxima:
+    print("¡Error! La pieza excede la capacidad máxima de la impresora (500 ml).")
+    print("Sugerencia: Divida el modelo en partes más pequeñas.")
+else:
+    costo_ml = float(input("Ingrese el costo de la resina por ml ($): "))
+    costo_total = volumen * costo_ml
+    print("Estado: ¡Aprobado para impresión!")
+    print(f"El costo total será: ${costo_total:.2f}")
